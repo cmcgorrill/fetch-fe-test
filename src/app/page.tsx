b7dd3d4833515ab375ff5ef35f5478dcb3a7b0e0
Courtney McGorrill <courtney@Courtneys-MacBook-Pro.local>
2025-02-11 03:09:32 -0500

    add login flow back in to the mix for 'demoability'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 'use client'
 
-// import { useEffect, useState } from "react";
-// import { LoginView } from "./components/login";
+import { useState } from "react";
+import { LoginView } from "./components/login";
 import { Box } from "@radix-ui/themes";
 import { DogPicker } from "./components/dogPicker";
 
 export default function Home() {
-  // const [isAuthed, setIsAuthed] = useState<boolean>(false)
-  // DONOW - connect flows to mimic a demo of full flow
+  const [isAuthed, setIsAuthed] = useState<boolean>(false)
   return (
     <Box>
-      {/* {isAuthed ? <DogPicker isAuthed={isAuthed} /> : <LoginView setIsAuthed={setIsAuthed} />} */}
+      {isAuthed ? <DogPicker isAuthed={isAuthed} /> : <LoginView setIsAuthed={setIsAuthed} />}
       <DogPicker isAuthed={true} />
     </Box>
   );
